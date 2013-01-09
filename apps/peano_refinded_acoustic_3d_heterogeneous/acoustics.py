304cbb7ad8d999becb2695b1ba6fff535d2887d4
Arash Bakhtiari <bakhtiar@in.tum.de>
2013-01-09 20:48:57 +0100

    Modifying the acoustic example to work with .obj files scenarios

--- apps/peano_refinded_acoustic_3d_heterogeneous/acoustics.py
+++ apps/peano_refinded_acoustic_3d_heterogeneous/acoustics.py
@@ -3,44 +3,92 @@
 
 import numpy as np
 import clawpack.peanoclaw as peanoclaw
+import wavefront as wf
 
-def init(state):
+OBJECTS = None
+XI = -1.0
+XF = 1.0
+LENGTH = XF - XI
+SUBDIVISION_FACTOR = 6
+CELLS = 3
+INIT_MIN_MESH_WIDTH = LENGTH / CELLS / SUBDIVISION_FACTOR
+
+def init_objects(filename):
+    global OBJECTS
+    OBJECTS = wf.read_obj(filename)
+
+def init_aux(state):
+    global OBJECTS
     zr = 2.0  # Impedance in right half
     cr = 2.0  # Sound speed in right half
 
-     zl = 1.0  # Impedance in left half
+    zl = 1.0  # Impedance in left half
     cl = 1.0  # Sound speed in left half
+
+    if OBJECTS is not None:
+        vertices = OBJECTS[0].vertices
+        vert_x = [vert[0] for vert in vertices ]
+        vert_y = [vert[1] for vert in vertices ]
+        vert_z = [vert[2] for vert in vertices ]
+        XMAX = max(vert_x)
+        XMIN = min(vert_x)
+        YMAX = max(vert_y)
+        YMIN = min(vert_y)
+        ZMAX = max(vert_z)
+        ZMIN = min(vert_z)
 
     grid = state.grid
     grid.compute_c_centers()
     X,Y,Z = grid._c_centers
+    
+    print 'XMIN',  XMIN
+    print 'XMAX',  XMAX
+    inside_the_object = np.logical_and(X > XMIN , X < XMAX)
+    state.aux[0,:,:,:] = zl*(np.logical_not(inside_the_object)) + zr*(inside_the_object) # Impedance
+    state.aux[1,:,:,:] = cl*(np.logical_not(inside_the_object)) + cr*(inside_the_object) # Sound speed
 
-    border = 0.35
-    state.aux[0,:,:,:] = zl*(X<border) + zr*(X>=border) # Impedance
-    state.aux[1,:,:,:] = cl*(X<border) + cr*(X>=border) # Sound speed
+    # border = 0.35
+    # state.aux[0,:,:,:] = zl*(X<border) + zr*(X>=border) # Impedance
+    # print state.aux[0,:,:,:]
+    # state.aux[1,:,:,:] = cl*(X<border) + cr*(X>=border) # Sound speed
+
+
+
+def init_q(state):
+    grid = state.grid
+    grid.compute_c_centers()
+    X,Y,Z = grid._c_centers
 
     x0 = -0.5; y0 = 0.0; z0 = 0.0
     r = np.sqrt((X-x0)**2 + (Y-y0)**2 + (Z-z0)**2)
     width=0.07
     state.q[0,:,:,:] = (np.abs(r-0.3)<=width)*(1.+np.cos(np.pi*(r-0.3)/width))
-
     state.q[1,:,:,:] = 0.
     state.q[2,:,:,:] = 0.
     state.q[3,:,:,:] = 0.
 
+
+def init(state):
+    init_aux(state)
+    init_q(state)
+
 def refinement_criterion(state):
+    global SUBDIVISION_FACTOR
+    global XI
+    global XF
+    global LENTGH
+    global CELLS
+    global INIT_MIN_MESH_WIDTH
     grid = state.grid;
     # num_dim = grid.num_dim
     delta = grid.delta
     xwidth = delta[0]
     print "DELTA: ", xwidth
-    aux = state.aux    
-    if aux[0,:,:,:].max() > 1:
+    if state.aux[0,:,:,:].max() > 1:
         print "REFINEMEN"
-        return 2/54.0
+        return INIT_MIN_MESH_WIDTH / 2.0
  
-    print "NO REFINEMENT"
-    return 2.0/18.0
+    return INIT_MIN_MESH_WIDTH
  
 def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',solver_type='classic',**kwargs):
     """
@@ -49,7 +97,12 @@ def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',so
     #===========================================================================
     # Import libraries
     #===========================================================================
-
+    global SUBDIVISION_FACTOR
+    global XI
+    global XF
+    global LENTGH
+    global CELLS
+    global INIT_MIN_MESH_WIDTH
     if use_petsc:
         import clawpack.petclaw as pyclaw
     else:
@@ -58,7 +111,6 @@ def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',so
     #===========================================================================
     # Setup solver and solver parameters
     #===========================================================================
-    subdivisionFactor = 6
     if solver_type=='classic':
         solver=pyclaw.ClawSolver3D()
     else:
@@ -67,8 +119,8 @@ def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',so
     from clawpack import riemann
 
     # Peano Solver
-    num_cells = 3.0
-    peanoSolver = peanoclaw.Solver(solver, (2./num_cells)/subdivisionFactor, 
+    peanoSolver = peanoclaw.Solver(solver,
+                                   INIT_MIN_MESH_WIDTH,
                                    init, 
                                    refinement_criterion=refinement_criterion
                                    )
@@ -100,12 +152,12 @@ def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',so
     #===========================================================================
 
     # Initialize domain
-    mx = subdivisionFactor
-    my = subdivisionFactor
-    mz = subdivisionFactor
-    x = pyclaw.Dimension('x', -1.0, 1.0, mx)
-    y = pyclaw.Dimension('y', -1.0, 1.0, my)
-    z = pyclaw.Dimension('z', -1.0, 1.0, mz)
+    mx = SUBDIVISION_FACTOR
+    my = SUBDIVISION_FACTOR
+    mz = SUBDIVISION_FACTOR
+    x = pyclaw.Dimension('x', XI, XF, mx)
+    y = pyclaw.Dimension('y', XI, XF, my)
+    z = pyclaw.Dimension('z', XI, XF, mz)
     domain = pyclaw.Domain([x,y,z])
 
     num_eqn = 4
@@ -148,10 +200,11 @@ def acoustics3D(iplot=False,htmlplot=False,use_petsc=False,outdir='./_output',so
     #===========================================================================
     # Plot results
     #===========================================================================
-    if htmlplot:  pyclaw.plot.html_plot(outdir=outdir,file_format=claw.output_format)
-    if iplot:     pyclaw.plot.interactive_plot(outdir=outdir,file_format=claw.output_format)
+    #if htmlplot:  pyclaw.plot.html_plot(outdir=outdir,file_format=claw.output_format)
+    #if iplot:     pyclaw.plot.interactive_plot(outdir=outdir,file_format=claw.output_format)
 
 if __name__=="__main__":
     import sys
     from clawpack.pyclaw.util import run_app_from_main
+    init_objects('cube.obj')
     output = run_app_from_main(acoustics3D)
